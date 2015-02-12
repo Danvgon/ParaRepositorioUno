0184f87c319b5ae36dfa27b9a9a37de6354b53d0
Daniel <Daniel@AdminW8>
2015-02-12 13:56:12 +0100

    Cambiamos el nombre de las variables

--- Prueba02/src/clase01.java
+++ Prueba02/src/clase01.java
@@ -1,6 +1,24 @@
 
 public class clase01 {
-int numero;
-int cosa;
+int numerouno;
+int cosauno;
 String letra;
+public int getNumero() {
+	return numerouno;
+}
+public void setNumero(int numero) {
+	this.numerouno = numero;
+}
+public int getCosa() {
+	return cosauno;
+}
+public void setCosa(int cosa) {
+	this.cosauno = cosa;
+}
+public String getLetra() {
+	return letra;
+}
+public void setLetra(String letra) {
+	this.letra = letra;
+}
 }
