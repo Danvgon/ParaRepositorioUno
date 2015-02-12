b899721b2ff248e4683bada2b40af02815d2553b
Daniel <Daniel@AdminW8>
2015-02-12 13:36:31 +0100

    Revert "Añadimos getters, setters y constructor"
    This reverts commit c397111d030acb3de3937446a7a7c9885566e916.

--- Prueba02/src/clase01.java
+++ Prueba02/src/clase01.java
@@ -1,37 +1,6 @@
+
 public class clase01 {
-	int numero;
-	int cosa;
-	String letra;
-
-	public clase01(int numero, int cosa, String letra) {
-		super();
-		this.numero = numero;
-		this.cosa = cosa;
-		this.letra = letra;
-	}
-
-	public int getNumero() {
-		return numero;
-	}
-
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-
-	public int getCosa() {
-		return cosa;
-	}
-
-	public void setCosa(int cosa) {
-		this.cosa = cosa;
-	}
-
-	public String getLetra() {
-		return letra;
-	}
-
-	public void setLetra(String letra) {
-		this.letra = letra;
-	}
-
+int numero;
+int cosa;
+String letra;
 }
